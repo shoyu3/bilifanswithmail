f4f371c250f0faa34f74110924b455c1cf69e0e4
shoyu3 <shoyu396@gmail.com>
2021-02-17 03:15:46 +0800

    Update bilifanswithmail.py

--- bilifanswithmail.py
+++ bilifanswithmail.py
@@ -5,47 +5,38 @@ from email.header import Header
 from datetime import datetime,timedelta
 #运行时如果报错先在cmd里运行pip install requests
 import requests
-import subprocess
+import os
 import json
-import ctypes
 import time
-
-try:
-    conf=open('biliconf.txt','r')
-except:
-    print('请先在目录下放入格式正确的biliconf.txt！按下回车退出')
-    input()
-    exit()
-conf2=conf.read()
-biliconf=json.loads(conf2)
 
 version='v0.1'
 #----------#
-uid=biliconf.get('uid')
-ctypes.windll.kernel32.SetConsoleTitleW('粉丝数监测 uid：'+str(uid))
+uid=input('请输入UID：')
+if uid == "":
+    #UID初始值，填入自己的UID即可
+    uid='229778960'
+os.system('title 粉丝数监测 uid：'+str(uid))
 url="https://api.bilibili.com/x/relation/stat?vmid="+uid
 url2 = 'https://api.bilibili.com/x/space/acc/info?mid='+uid
 fansold=0
-delay=int(biliconf.get('delay'))  #刷新冷却 单位：秒
+delay=30  #刷新冷却 单位：秒
 now = datetime.now()
 #以下为日志文件保存名称，可自行更改
-logsave=biliconf.get('logsave')
-if logsave == "":
-    logsave='log'+now.strftime("%Y%m%d-%H%M")+'.txt'
+logsave='log'+now.strftime("%Y%m%d-%H%M")+'.txt'
 
 #发送和接收最好都使用QQ邮箱，且需要在手机上安装QQ邮箱客户端并登录接收用邮箱
-mail_host=biliconf.get('mailhost')  #设置SMTP服务器，如使用QQ邮箱发送则无需更改
-mail_user=biliconf.get('mailuser')  #用户名，即邮箱地址
-mail_pass=biliconf.get('mailpass')  #密码，在网页端QQ邮箱启用smtp服务并将生成的授权码填入此处
-sender = biliconf.get('sender')  #发送方，应和上面的邮箱地址相同
-receivers = [biliconf.get('receiver')]  #接收邮件，可设置为你的QQ邮箱或者其他邮箱
+mail_host="smtp.qq.com"  #设置SMTP服务器，如使用QQ邮箱发送则无需更改
+mail_user="***@qq.com"  #用户名，即邮箱地址
+mail_pass="ill***dchf"  #密码，在网页端QQ邮箱启用smtp服务并将生成的授权码填入此处
+sender = '***@qq.com'  #发送方，应和上面的邮箱地址相同
+receivers = ['***@qq.com']  #接收邮件，可设置为你的QQ邮箱或者其他邮箱
 #----------#
 
 # 第三方 SMTP 服务，使用加密协议，下面的“粉丝变化通报”标题可自行修改
 def send():
     message = MIMEText(text, 'plain', 'utf-8')
-    message['From'] = Header(biliconf.get('from'), 'utf-8')
-    message['To'] =  Header(biliconf.get('to'), 'utf-8')
+    message['From'] = Header("粉丝变化通报", 'utf-8')
+    message['To'] =  Header("shoyu", 'utf-8')
     subject = text
     message['Subject'] = Header(subject, 'utf-8')
     try:
@@ -70,7 +61,7 @@ jdata2=json_dict2['data']
 name=jdata2.get('name')
 now = datetime.now()
 nowtime=now.strftime("%Y-%m-%d %H:%M:%S")
-ctypes.windll.kernel32.SetConsoleTitleW('粉丝数实时监测 '+name+' uid：'+str(uid)+' 当前粉丝数：'+str(fansold))
+os.system('title 粉丝数实时监测 '+name+' uid：'+str(uid)+' 当前粉丝数：'+str(fansold))
 print('粉丝数监测 '+version+' 网页版地址：https://shoyu.top/bili')
 print('系统初始化，启动时间：'+nowtime+' UID：'+str(uid)+' 用户名：'+name+' 起始粉丝数：'+str(fansold))
 print('监测日志将保存在：'+logsave)
@@ -104,7 +95,7 @@ while True:
             nowtime=now.strftime("%Y-%m-%d %H:%M:%S")
             logtxt.write('[ '+nowtime+' ] '+text)
             logtxt.write('\n')
-            ctypes.windll.kernel32.SetConsoleTitleW('粉丝数实时监测 '+name+' uid：'+str(uid)+' 当前粉丝数：'+str(fans))
+            os.system('title 粉丝数实时监测 '+name+' uid：'+str(uid)+' 当前粉丝数：'+str(fans))
             send()
         fansold=fans
     except:
@@ -113,4 +104,3 @@ while True:
         logtxt.write('\n')
     logtxt.close()
     time.sleep(delay)
-
